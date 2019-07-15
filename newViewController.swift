fedde5d36326f4e3a4729786cef88f313e399cec
Alexander Clark <45522968+alexclark2@users.noreply.github.com>
2019-07-15 15:13:12 -0500

    New view Controller

---
+++ newViewController.swift
@@ -0,0 +1,18 @@
+//
+//  newViewController.swift
+//  JETiOS
+//
+//  Created by MACBOOK on 7/15/19.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class newViewController: UIViewController {
+    
+    
+    override func viewDidLoad(){
+        
+    }
+}
